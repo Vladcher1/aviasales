d485be300042f6810acce7f6af357c56d2180539
Vlada Cherepanova <vlada.hero@inbox.ru>
2023-01-02 15:07:25 +0500

    ready html and scss

--- src/components/side-filter/side-filter.tsx
+++ src/components/side-filter/side-filter.tsx
@@ -7,34 +7,37 @@ const SideFilter = () => {
       <form className="side-filter__form">
         <div className="side-filter__filter-item">
           <label className="side-filter__label">
-            <input className="changed" type="checkbox" />
+            <input className="checkbox" type="checkbox"></input>
             <span className="custom-checkbox "></span>
-            Все
+            <span className="checkbox-name ">Все</span>
           </label>
         </div>
         <div className="side-filter__filter-item">
           <label>
-            <input className="checkbox" type="checkbox" />
+            <input className="checkbox" type="checkbox"></input>
             <span className="custom-checkbox "></span>
-            Без пересадок
+            <span className="checkbox-name ">Без пересадок</span>
           </label>
         </div>
         <div className="side-filter__filter-item">
           <label>
-            <input className="checkbox" type="checkbox" />
-            <span className="custom-checkbox "></span>1 пересадка
+            <input className="checkbox" type="checkbox"></input>
+            <span className="custom-checkbox "></span>
+            <span className="checkbox-name ">1 пересадка</span>
           </label>
         </div>
         <div className="side-filter__filter-item">
           <label>
-            <input className="checkbox" type="checkbox" />
-            <span className="custom-checkbox "></span>2 пересадки
+            <input className="checkbox" type="checkbox"></input>
+            <span className="custom-checkbox "></span>
+            <span className="checkbox-name ">2 пересадки</span>
           </label>
         </div>
         <div className="side-filter__filter-item">
           <label>
-            <input className="checkbox" type="checkbox" />
-            <span className="custom-checkbox "></span>3 пересадки
+            <input className="checkbox" type="checkbox"></input>
+            <span className="custom-checkbox "></span>
+            <span className="checkbox-name ">3 пересадки</span>
           </label>
         </div>
       </form>
