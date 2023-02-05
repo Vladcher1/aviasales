8effe302d98dfaaa91bdbfa5b47bc78f3c508d27
Vlada Cherepanova <vlada.hero@inbox.ru>
2023-02-06 00:12:51 +0500

    side filter refactored

--- src/components/side-filter/side-filter.tsx
+++ src/components/side-filter/side-filter.tsx
@@ -2,56 +2,45 @@ import React from "react";
 import "./side-filter.scss";
 
 import FilterItem from "../filter-item/filter-item";
-import { makeId } from "../../utilities";
 import {
   filterOptionsObject,
   filterOptionsTitleObject,
 } from "../../constants/constants";
 
-const newId = makeId();
-
 const filtersArray = [
   {
     payload: filterOptionsObject.ALL,
-    id: newId(),
     title: filterOptionsTitleObject.ALL_TITLE,
   },
   {
     payload: filterOptionsObject.NO_CONNECTING_FLIGHTS,
-    id: newId(),
     title: filterOptionsTitleObject.NO_CONNECTING_FLIGHTS_TITLE,
   },
   {
     payload: filterOptionsObject.ONE_CONNECTING_FLIGHTS,
-    id: newId(),
     title: filterOptionsTitleObject.ONE_CONNECTING_FLIGHTS_TITLE,
   },
   {
     payload: filterOptionsObject.TWO_CONNECTING_FLIGHTS,
-    id: newId(),
     title: filterOptionsTitleObject.TWO_CONNECTING_FLIGHTS_TITLE,
   },
   {
     payload: filterOptionsObject.THREE_CONNECTING_FLIGHTS,
-    id: newId(),
     title: filterOptionsTitleObject.THREE_CONNECTING_FLIGHTS_TITLE,
   },
 ] as const;
 
-const SideFilter: React.FC = () => {
-  const filterItems = filtersArray.map((obj) => {
-    const { payload, id, title } = obj;
-    return <FilterItem title={title} payload={payload} key={id} />;
-  });
-
-  return (
-    <div className="side-filter">
-      <h5 className="side-filter__title">Количество пересадок</h5>
-      <form className="side-filter__form">
-        <ul>{filterItems}</ul>
-      </form>
-    </div>
-  );
-};
+const SideFilter: React.FC = () => (
+  <div className="side-filter">
+    <h5 className="side-filter__title">Количество пересадок</h5>
+    <form className="side-filter__form">
+      <ul>
+        {filtersArray.map(({ payload, title }) => (
+          <FilterItem title={title} payload={payload} key={title} />
+        ))}
+      </ul>
+    </form>
+  </div>
+);
 
 export default SideFilter;
